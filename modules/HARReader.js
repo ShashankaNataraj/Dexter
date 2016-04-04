1ac1bf0af0031bd6009d4ecc613560dfcf39368d
Shashanka Nataraj <shashankan.10@gmail.com>
2016-04-04 20:29:14 +0530

    Defined class skeleton new sh script to trigger server

--- modules/HARReader.js
+++ modules/HARReader.js
@@ -1,21 +1,28 @@
 'use strict';
 class HARReader {
+    constructor() {
+        this.httpPaths = {};
+    }
+
     /**
      * Opens the HAR file and calls other utility methods
      * */
-    readHAR(filePath) {
-        this.readHttpPaths(filePath);
+    readHar() {
+        this.readHttpPaths(this.filePath);
+        return 1;
     }
+
     /**
      * Reads paths from the HAR file and keeps them in memory
      * */
-    readHttpPaths(filePath){
-        
+    readHttpPaths() {
+
     }
+
     /**
      * Reads data for a given HTTP path
      * */
-    readHttpData(){
+    readHttpData() {
 
     }
 }
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 1121;
-var harReader = require('./modules/HARReader.js');
+
+var fs = require('fs');
+var parseArgs = require('minimist');
 
 
 app.listen(port, ()=>{//Start the server and listen on a port
+    var argv = parseArgs((process.argv));
+    var obj = JSON.parse(fs.readFileSync('sample.har', 'utf8'));
+    var harReader = require('./modules/HARReader.js')(obj);
+    console.log(harReader.readHar(obj));
     console.log('Running Yama at:' + port);
 });
