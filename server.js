02abffa47e0d19fb197031c2c9b0d47e12650e22
Shashanka Nataraj <shashankan.10@gmail.com>
2016-04-23 22:22:21 +0530

    ES6 refactor with use stricts and proper logic

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 1121;
-var parseArgs = require('minimist');
-var harReader = require('./modules/HARReader.js');
+'use strict';
+let express = require('express'),
+    app = express(),
+    port = process.env.PORT || 1121,
+    url = require('url'),
+    parseArgs = require('minimist'),
+    routes = require('./modules/Routes.js'),
+    harReader = require('./modules/HarReader.js'),
+    har = new harReader();
 
-
-app.listen(port, ()=>{//Start the server and listen on a port
-    
-    var har = new harReader();
+app.listen(port, ()=>{ //Start the server and listen on a port
     har.readHar('sample.har');
     console.log('Running Yama at:' + port);
+});
+app.get('/*', function(req, res) {
+    har.getResponseForUrl(url.parse(req.url).pathname);
 });
