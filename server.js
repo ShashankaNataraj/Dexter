682fb58c38f64285a002749be1a1a27154a72b37
Shashanka Nataraj <shashankan.10@gmail.com>
2016-07-14 08:12:02 +0530

    Fixed logic to get the object from the hash map

--- server.js
+++ server.js
@@ -3,21 +3,37 @@ let express = require('express'),
     app = express(),
     port = process.env.PORT || 1121,
     parseArgs = require('minimist'),
-    routes = require('./Modules/Routes.js'),
-    harReader = require('./Modules/HarReader.js'),
-    har = new harReader();
+    routes = require('./modules/Routes.js'),
+    harReader = require('./modules/HarReader.js'),
+    har = new harReader(),
+    colors = require('colors');
 
 app.listen(port, ()=> { //Start the server and listen on a port
     har.readHar('sample.har');
-    console.log('Running Yama at:' + port);
+    console.log(colors.green.bold('Started Yama at:' + port));
+});
+app.get('/favicon.ico', function (request, response) {
+    response.end();
 });
 app.get('/*', (request, response) => {
     let storedResponse;
+    let url = request.url;
+    console.log(colors.yellow('Received request for:' + url));
     try {
+        console.log('inside');
         storedResponse = har.getResponseForUrl(request.url);
-        response.setHeader('Content-Type', storedResponse.content.mimeType);
-        response.setHeader('Content-Type', storedResponse.content.mimeType);
+
+        console.log(storedResponse);
+        storedResponse.headers.forEach(function (header) {
+            response.setHeader(header.name, header.value)
+        });
+        response.write('asd');
+        // storedResponse.headers.forEach(({word, count}) => {
+        //     console.log(word+' '+count);
+        // });
     } catch (e) {
         response.send(e.message);
+    } finally {
+        response.end();
     }
 });
