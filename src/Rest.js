c5ef6b529e1b3f1ccfb3fa30ed2877c580ea97c7
Shashanka Nataraj <shashankan.10@gmail.com>
2016-07-17 20:42:27 +0530

    Component: Build script and coveralls integration
    Testing codeship
    
    #3

--- src/Rest.js
+++ src/Rest.js
@@ -1,10 +1,10 @@
 describe('Rest API', () => {
 
     before(() => {
-        // runs before all tests in this block
+
     });
 
     after(() => {
-        // runs after all tests in this block
+        
     });
 });
