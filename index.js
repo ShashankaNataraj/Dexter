727a990d028cbe18ef5c7a7961db94cb8293daef
Shashanka Nataraj <shashankan.10@gmail.com>
2016-07-18 09:30:42 +0530

    Convert Dexter into a class based structure
    For readability and testability
    
    Closes #5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
+
 /**
- * Requires
+ * Requires and consts
  * */
 const
     express = require('express'),
@@ -9,81 +10,94 @@ const
     harReader = require('./src/HarReader'),
     harUtils = require('./src/HarUtils');
 
-/**
- * Lets for later usage
- * */
-const
-    app = express(),
-    port = process.env.PORT || 1121,
-    har = new harReader(),
-    utils = new harUtils();
-
-/**
- * Generic handler for all HTTP methods
- * */
-function genericHandler(request, response, method) {
-    let storedResponse;
-    let url = request.url;
-    process.stdout.write(colors.yellow('Serving ' + url + ':'));
-    try {
-        storedResponse = har.getResponseForUrl(request.url, method);
-    } catch (e) {
-        process.stdout.write(colors.red('Didn\'t find corresponding entry in HAR, returning 404\n'));
-        response.statusCode = 404;
-        response.send(e.message);
+class Dexter {
+    constructor(harPath) {
+        this._app = express();
+        this._port = process.env.PORT || 1121;
+        this._har = new harReader();
+        this._utils = new harUtils();
+        this._harPath = harPath;
     }
-    if (storedResponse) {
-        process.stdout.write(colors.green('Found entry, writing response with headers and cookies... '));
-        // Set the status
-        response.status(storedResponse.status);
-        // Set the headers
-        let headers = utils.filterHeaders(storedResponse.headers);
-        headers.forEach((header)=> {
-            response.setHeader(header.name, header.value)
+
+    /**
+     * Handles entire server startup process (aka Ooh what does this button do)
+     * */
+    startUp() {
+        this.attachMethodHandlers();
+        this.serveStaticFiles();
+        this.startServer();
+    }
+
+    /**
+     * Start listening
+     * */
+    startServer() {
+        this.server = this._app.listen(this._port, ()=> { //Start the server and listen on a port
+            this._har.readHar(this._harPath);
+            console.log(colors.green.bold('Started Dexter at:' + this._port));
         });
-        // Set Cookies
-        storedResponse.cookies.forEach((cookie)=> {
-            response.cookie(
-                cookie.name,
-                cookie.value,
-                utils.processCookie(cookie)
-            );
+    }
+
+    /**
+     * Attaches HTTP method handlers to incoming requests
+     * */
+    attachMethodHandlers() {
+        this._app.all('/*', (request, response)=> {
+            this.genericHandler(request, response, request.method);
         });
-        // Send the text content
-        response.send(storedResponse.content.text);
-        process.stdout.write(colors.green('Done \n'));
-        response.end();
+    }
+
+    /**
+     * Serve static files
+     */
+    serveStaticFiles() {
+        this._app.use(express.static('public'));
+    }
+
+    /**
+     * Generic handler for all HTTP methods
+     * */
+    genericHandler(request, response, method) {
+        let storedResponse;
+        let url = request.url;
+        process.stdout.write(colors.yellow('Serving ' + url + ':'));
+        try {
+            storedResponse = this._har.getResponseForUrl(request.url, method);
+        } catch (e) {
+            process.stdout.write(colors.red('Didn\'t find corresponding entry in HAR, returning 404\n'));
+            response.statusCode = 404;
+            response.send(e.message);
+        }
+        if (storedResponse) {
+            process.stdout.write(colors.green('Found entry, writing response with headers and cookies... '));
+            // Set the status
+            response.status(storedResponse.status);
+            // Set the headers
+            let headers = this._utils.filterHeaders(storedResponse.headers);
+            headers.forEach((header)=> {
+                response.setHeader(header.name, header.value)
+            });
+            // Set Cookies
+            storedResponse.cookies.forEach((cookie)=> {
+                response.cookie(
+                    cookie.name,
+                    cookie.value,
+                    this._utils.processCookie(cookie)
+                );
+            });
+            // Send the text content
+            response.send(storedResponse.content.text);
+            process.stdout.write(colors.green('Done \n'));
+            response.end();
+        }
+    }
+
+    /**
+     * Dee Dee :O
+     * */
+    tearDown() {
+        this.server.close();
     }
 }
-app.post('/*', (request, response)=> {
-    genericHandler(request, response, 'POST');
-});
-app.get('/*', (request, response)=> {
-    genericHandler(request, response, 'GET');
-});
-app.put('/*', (request, response)=> {
-    genericHandler(request, response, 'PUT');
-});
-app.delete('/*', (request, response)=> {
-    genericHandler(request, response, 'DELETE');
-});
-app.connect('/*', (request, response)=> {
-    genericHandler(request, response, 'CONNECT');
-});
-app.head('/*', (request, response)=> {
-    genericHandler(request, response, 'HEAD');
-});
 
-
-/**
- * Serve static files
- */
-app.use(express.static('public'));
-
-/**
- * Start listening
- * */
-app.listen(port, ()=> { //Start the server and listen on a port
-    har.readHar('test/data/sample.har');
-    console.log(colors.green.bold('Started Dexter at:' + port));
-});
+new Dexter('test/data/sample.har').startUp();
